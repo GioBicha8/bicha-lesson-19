d66ba09842fa723d6f2abd671d35c69fad99fdff
GioBicha8 <g.bicha888@gmail.com>
2022-11-14 15:12:48 +0400

    html and.css done

---
+++ main.js
@@ -0,0 +1,8 @@
+const form = document.getElementById("form");
+const personal = document.getElementById("personal");
+const mobile = document.getElementById("mobile");
+const job = document.getElementById("job");
+
+form.addEventListener("submit", (e) => {
+  console.log(e);
+});
